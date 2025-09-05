e0243b7b0418060f1007cc2145ff83c7fbba563b
Mavis Atayo Adombire <145336116+madombire@users.noreply.github.com>
2025-09-05 14:35:52 +0200

    Completed work on updating the time

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-alert("Hello World");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector("#date");
+  let losAngelesTimeElement = document.querySelector("#time");
+
+  // Update date and time for Los Angeles
+  losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM, Do YYYY");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = document.querySelector("#paris-date");
+  let parisTimeElement = document.querySelector("#paris-time");
+
+  parisDateElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("MMMM, Do YYYY");
+
+  parisTimeElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
